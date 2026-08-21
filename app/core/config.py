c8f918cf1a1b49786be97b7e47cc574dcae39af8
Shivam Khokhani <sxk2125@mavs.uta.edu>
2026-08-21 13:46:55 -0500

    Add application configuration and dependencies

---
+++ app/core/config.py
@@ -0,0 +1,2 @@
+app_name = "ChannelGateway"
+environment = "development"
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from app.core import config
 
-app = FastAPI()
+app = FastAPI(title=config.app_name)
 
 @app.get("/health")
 def status():
