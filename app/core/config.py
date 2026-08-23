2c47cd665aecc66006054195cdf15920101302ba
Shivam Khokhani <sxk2125@mavs.uta.edu>
2026-08-23 14:29:20 -0500

    Initial db setup

--- app/core/config.py
+++ app/core/config.py
@@ -1,2 +1,28 @@
-app_name = "ChannelGateway"
-environment = "development"
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+
+    APP_NAME: str = "Channel Gateway"
+    ENVIRONMENT: str = "development"
+    DEBUG: bool = False
+
+    DATABASE_HOST : str = "localhost"
+    DATABASE_PORT : int = 5434
+    DATABASE_USER : str = "channel"
+    DATABASE_PASSWORD : str = "channel"
+    DATABASE_NAME : str ="channel_gateway"
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
+
+    @property
+    def database_url(self) -> str:
+        return (
+            f"postgresql+psycopg://"
+            f"{self.DATABASE_USER}:{self.DATABASE_PASSWORD}"
+            f"@{self.DATABASE_HOST}:{self.DATABASE_PORT}"
+            f"/{self.DATABASE_NAME}"
+        )
+settings = Settings()
---
+++ app/db/session.py
@@ -0,0 +1,12 @@
+from app.core.config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(settings.database_url)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
